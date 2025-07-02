02c2e96c7260bc626fed3e4849d0c3fedab139c3
chanhcon <hoanntnpn00099@gmail.com>
2025-07-02 22:56:40 +0700

    edit components message

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -18,7 +18,7 @@
     <div class="fixed top-4 right-4 z-50">
         <div class="bg-red-500 text-white px-6 py-3 rounded-lg shadow-lg flex items-center space-x-2">
             <svg class="w-5 h-5" fill="none" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
             <span>{{ session('error') }}</span>
             <button onclick="this.parentElement.remove()" class="ml-2 hover:text-red-200">
@@ -28,5 +28,5 @@
             </button>
         </div>
     </div>
-@endif
+@endif 
 
